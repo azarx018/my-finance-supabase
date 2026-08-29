ad9f5ea55438332ee7431db62095701737243be9
azarx018 <zharr188@gmail.com>
2026-08-29 20:45:57 +0700

    VibeSync: Baseline

--- src/lib/data/debt.ts
+++
@@ -1,6 +0,0 @@
-export function daysUntil(dateStr: string): number {
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  const target = new Date(dateStr + 'T00:00:00');
-  return Math.round((target.getTime() - today.getTime()) / 86400000);
-}
